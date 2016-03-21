09b7aca3224ac9e858eec11b7f713a7ed36f03a0
siphesande <siphesande@projectcodex.co>
2016-03-21 10:46:48 +0200

    node

--- node-express/server.js
+++ node-express/server.js
@@ -24,63 +24,63 @@
 
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
-var express = require('express');
-var morgan = require('morgan');
+// var express = require('express');
+// var morgan = require('morgan');
 
-var hostname = 'localhost';
-var port = 3000;
+// var hostname = 'localhost';
+// var port = 3000;
 
-var app = express();
+// var app = express();
 
-app.use(morgan('dev'));
-
-
-
-var dR = require('./dishRouter');
-var dishRouter;
-
-dR(express,function(err,dishRouterObj) {
-        if (err) {
-	    console.log(err);
-	}
-	else {
-	    dishRouter=dishRouterObj.getdishRouter();
-	}
-    });
-
-var pR = require('./promoRouter');
-var promoRouter;
-
-pR(express,function(err,promoRouterObj) {
-        if (err) {
-	    console.log(err);
-	}
-	else {
-	    promoRouter=promoRouterObj.getpromoRouter();
-	}
-    });
-
-
-var lR = require('./leaderRouter');
-var leaderRouter;
-
-lR(express,function(err,leaderRouterObj) {
-        if (err) {
-	    console.log(err);
-	}
-	else {
-	    leaderRouter=leaderRouterObj.getleaderRouter();
-	}
-    });
+// app.use(morgan('dev'));
 
 
 
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leadership',leaderRouter);
+// var dR = require('./dishRouter');
+// var dishRouter;
 
-app.use(express.static(__dirname + '/public'));
+// dR(express,function(err,dishRouterObj) {
+//         if (err) {
+// 	    console.log(err);
+// 	}
+// 	else {
+// 	    dishRouter=dishRouterObj.getdishRouter();
+// 	}
+//     });
 
-app.listen(port, hostname, function(){
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// var pR = require('./promoRouter');
+// var promoRouter;
+
+// pR(express,function(err,promoRouterObj) {
+//         if (err) {
+// 	    console.log(err);
+// 	}
+// 	else {
+// 	    promoRouter=promoRouterObj.getpromoRouter();
+// 	}
+//     });
+
+
+// var lR = require('./leaderRouter');
+// var leaderRouter;
+
+// lR(express,function(err,leaderRouterObj) {
+//         if (err) {
+// 	    console.log(err);
+// 	}
+// 	else {
+// 	    leaderRouter=leaderRouterObj.getleaderRouter();
+// 	}
+//     });
+
+
+
+// app.use('/dishes',dishRouter);
+// app.use('/promotions',promoRouter);
+// app.use('/leadership',leaderRouter);
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.listen(port, hostname, function(){
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
