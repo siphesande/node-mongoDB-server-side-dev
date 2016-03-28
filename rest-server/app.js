697f161b7aacdc1f46e336d01847a6daf35a75a4
siphesande <siphesande@projectcodex.co>
2016-03-28 14:31:06 +0200

    server-side

--- rest-server/app.js
+++ rest-server/app.js
@@ -7,8 +7,23 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
+var mongoose = require('mongoose');
+
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+});
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,14 +31,26 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leadership',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,7 +60,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -55,6 +81,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- week2/models/dishes.js
+++ week2/models/dishes.js
@@ -1,45 +1,10 @@
-// // grab the things we need
-// var mongoose = require('mongoose')
-// require('mongoose-currency').loadType(mongoose)
-// var Schema = mongoose.Schema
-// var Currency = mongoose.Types.Currency
-
-// /* Comment Schema */
-// var commentSchema = new Schema({
-//   rating: { type: Number, min: 1, max: 5, required: true },
-//   comment: { type: String, required: true },
-//   author: { type: String, required: true }
-// }, {
-//   timestamps: true
-// })
-
-// /* Dish Schema */
-// var dishSchema = new Schema({
-//   name: { type: String, required: true, unique: true },
-//   image: { type: String, default: '' },
-//   category: { type: String, default: '' },
-//   label: { type: String, default: '' },
-//   price: { type: Currency, default: '' },
-//   description: { type: String, required: true },
-//   comments: [commentSchema]
-// }, {
-//   timestamps: true
-// })
-
-// // the schema is useless so far
-// // we need to create a model using it
-// var Dishes = mongoose.model('Dish', dishSchema)
-
-// // make this available to our Node applications
-// module.exports = Dishes
-
-// grab the things we need
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 // Will add the Currency type to the Mongoose Schema types
 require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
+//DEFINING A COMMENT SCHEMA
 var commentSchema = new Schema({
     rating:  {
         type: Number,
@@ -55,42 +20,50 @@ var commentSchema = new Schema({
         type: String,
         required: true
     }
-}, {
+},
+ {
     timestamps: true
 });
 
-// create a schema
+// define a schema
 var dishSchema = new Schema({
     name: {
         type: String,
         required: true,
         unique: true
     },
-    description: {
-        type: String,
-        required: true
-    },
+    
     image: {
         type: String,
-        required: true
+        required: true,
+        default: ""
     },
     category: {
         type: String,
-        required: true
+        required: true,
+        default: ''
     },
     label: {
         type: String,
         default: ""
     },
-    price: { type: Currency },
+    price: { 
+      type: Currency 
+    },
+    description: {
+        type: String,
+        required: true
+    },
     comments:[commentSchema]
-}, {
+},
+
+  {
     timestamps: true
 });
 
-// the schema is useless so far
-// we need to create a model using it
+
+//Creating a model 
 var Dishes = mongoose.model('Dish', dishSchema);
 
-// make this available to our Node applications
+/* make this available to our Node applications*/
 module.exports = Dishes;
