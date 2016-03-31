3775e519166c9c44aa2e9b2723aa2d84a96b584b
siphesande <siphesande@projectcodex.co>
2016-03-31 09:29:35 +0200

    User Authentication with Passport

---
+++ rest-server-passport/models/promotions.js
@@ -0,0 +1,63 @@
+// // grab the things we need
+// var mongoose = require('mongoose')
+// require('mongoose-currency').loadType(mongoose)
+// var Schema = mongoose.Schema
+// var Currency = mongoose.Types.Currency
+
+// /* Promotion Schema */
+// var promoSchema = new Schema({
+//   name: { type: String, required: true, unique: true },
+//   image: { type: String, default: '' },
+//   label: { type: String, default: '' },
+//   price: { type: Currency, default: '' },
+//   description: { type: String, required: true }
+// }, {
+//   timestamps: true
+// })
+
+// // the schema is useless so far
+// // we need to create a model using it
+// var Promotions = mongoose.model('Promotion', promoSchema)
+
+// // make this available to our Node applications
+// module.exports = Promotions
+
+
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Will add the Currency type to the Mongoose Schema types
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+// create a schema
+var promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ""
+    },
+    price: { type: Currency }
+}, {
+    timestamps: true
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Promotions = mongoose.model('Promotion', promotionSchema);
+
+// make this available to our Node applications
+module.exports = Promotions;
+
---
+++ rest-server-passport/routes/leaderRouter.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Leaderships = require('../models/leadership');
+
+var leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.get(function (req, res, next) {
+    Leaderships.find({}, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
+})
+
+.post(function (req, res, next) {
+    Leaderships.create(req.body, function (err, leader) {
+        if (err) throw err;
+        console.log('Dish created!');
+        var id = leader._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the leader with id: ' + id);
+    });
+})
+
+.delete(function (req, res, next) {
+    Leaderships.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+leaderRouter.route('/:leaderId')
+.get(function (req, res, next) {
+    Leaderships.findById(req.params.leaderId, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
+})
+
+.put(function (req, res, next) {
+    Leaderships.findByIdAndUpdate(req.params.leaderId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
+})
+
+.delete(function (req, res, next) {
+    Leaderships.findByIdAndRemove(req.params.leaderId, function (err, resp) { if (err) throw err;
+        res.json(resp);
+    });
+});
+
+module.exports = leaderRouter
