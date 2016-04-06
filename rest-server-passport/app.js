21902b37997863f212a05e9e7b67a38297dbf511
siphesande <siphesande@projectcodex.co>
2016-04-06 18:15:22 +0200

    authenticate.js

--- rest-server-passport/app.js
+++ rest-server-passport/app.js
@@ -1,12 +1,12 @@
-var express = require('express')
-var path = require('path')
+var express = require('express');
+var path = require('path');
 // var favicon = require('serve-favicon')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var authenticate = require('./authenticate');
 
 var config = require('./config');
 
@@ -48,11 +48,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 // passport config
-var User = require('./models/user');
+
 app.use(passport.initialize());
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 app.use(express.static(path.join(__dirname, 'public')))
 
--- rest-server-passport/models/user.js
+++ rest-server-passport/models/user.js
@@ -7,13 +7,15 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
     username: String,
     password: String,
+    OauthId: String,
+    OauthToken: String,
     firstname: {
       type: String,
-        default: ''
+      default: ''
     },
     lastname: {
       type: String,
-        default: ''
+      default: ''
     },
     admin:   {
         type: Boolean,
